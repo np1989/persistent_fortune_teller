bae5f74727bf00689c6717de32aa332615ce2447
np1989 <np1989@gmail.com>
2015-05-20 20:18:50 -0500

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get("/",        { :controller => "fortunes", :action => "lucky" })
+  get("/",{ :controller => "fortunes", :action => "lucky" })
 
-  get("/lucky_numbers",   { :controller => "fortunes", :action => "lucky" })
+  get("/lucky_numbers", { :controller => "fortunes", :action => "lucky" })
   get("/unlucky_numbers", { :controller => "fortunes", :action => "unlucky" })
 
   get("/random/:min/:max", { :controller => "fortunes", :action => "within_range" })
